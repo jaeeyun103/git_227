d58a0b92e37a0032c80c5810ac3daa365be076be
test <test@test.com>
2025-11-27 14:59:52 +0900

    [코드 병합 및 조정]

--- 코드_공유.py
+++ 코드_공유.py
@@ -32,8 +32,8 @@ from sklearn.tree import DecisionTreeClassifier
 
 param_grid = {
     'criterion': ['gini', 'entropy'],
-    'max_depth': [2, 5],
-    'min_samples_split': [2, 10],
+    'max_depth': [2, 3, 4, 5],
+    'min_samples_split': [2, 5, 10],
     'min_samples_leaf': [1, 2, 4]
 }
 
