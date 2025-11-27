9fc5e908ca244a6ebf1794f760fb238bc4217ea3
test <test@test.com>
2025-11-27 14:41:29 +0900

    [DT 모델링 코드 작성]

--- 코드_공유.py
+++ 코드_공유.py
@@ -17,13 +17,53 @@ wine = load_wine()
 # X, y 데이터를 test size는 0.2, random_state 값은 42로 하여 train 데이터와 test 데이터로 분할합니다.
 
 ''' 코드 작성 바랍니다 '''
+X = pd.DataFrame(wine.data, columns=wine.feature_names)
+y = pd.Series(wine.target)
 
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
 ####### A 작업자 작업 수행 #######
 
 ''' 코드 작성 바랍니다 '''
 
+from sklearn.tree import DecisionTreeClassifier
 
+param_grid = {
+    'criterion': ['gini', 'entropy'],
+    'max_depth': [2, 5],
+    'min_samples_split': [2, 10],
+    'min_samples_leaf': [1, 2, 4]
+}
+
+dt = DecisionTreeClassifier()
+
+grid_search = GridSearchCV(
+    dt,
+    param_grid,
+    cv=5,
+    scoring='accuracy',
+    n_jobs=-1
+)
+
+grid_search.fit(X_train, y_train)
+
+best_dt = grid_search.best_estimator_
+
+print("최적 하이퍼파라미터:", grid_search.best_params_)
+print("최적 모델 정확도(Train CV 기준):", grid_search.best_score_)
+print("테스트 정확도:", best_dt.score(X_test, y_test))
+
+importances = best_dt.feature_importances_
+feature_names = X.columns
+
+plt.figure(figsize=(10, 5))
+plt.barh(feature_names, importances)
+plt.xlabel("Feature Importance")
+plt.ylabel("Feature")
+plt.title("Decision Tree Feature Importance")
+plt.show()
 
 ####### B 작업자 작업 수행 #######
 
